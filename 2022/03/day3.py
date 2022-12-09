1b97806a81f79dca7d326747ab9d0c3e4a4cfa04
Christoffer Hjerne <c.hjerne@gmail.com>
2022-12-09 22:33:43 +0100

    Finish day 3

--- 2022/03/day3.py
+++ 2022/03/day3.py
@@ -1,14 +1,32 @@
 "day3"
-file = open("data.txt","r")
+file = open("data.txt", "r")
 bags = file.read().splitlines()
-occurances = []
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+upperalphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+                 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+                 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+totalprio = 0
+characterread = False
+
 for bag in bags:
+    characterread = False
     items = len(bag)
-    occurances.append(["pocketstart"])
-    pocket1 = bag[:items//2]  
-    occurances.append(pocket1)
-    occurances.append(["pocketmiddle"])
+    pocket1 = bag[:items//2]
     pocket2 = bag[items//2:]
-    occurances.append(pocket2)
-    occurances.append(["pocketend"])
-print(occurances)
+    for character in pocket1:
+        if character in pocket2 and characterread is False:
+            if character.islower():
+                prio = alphabet.index(character)
+                prio += 1
+                totalprio += prio
+                characterread = True
+            else:
+                prio = upperalphabet.index(character)
+                prio += 27
+                totalprio += prio
+                characterread = True
+
+
+print(totalprio)
